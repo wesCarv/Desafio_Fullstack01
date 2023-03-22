269639fd7270662171d7415d3c17d3ea70c13436
wesCarv <wcarvalho516@gmail.com>
2023-03-22 16:49:29 -0300

    feat: criação do service de delete de clients

---
+++ back-end/src/services/clients/deleteClientService.ts
@@ -0,0 +1,17 @@
+import AppDataSource from "../../data-source"
+import { Client } from "../../entities/clientEntity"
+import { AppError } from "../../errors/appError"
+
+export const deleteCLient = async (id: string) => {
+    const userRepository = AppDataSource.getRepository(Client)
+
+    const findClient = await userRepository.findOneBy({id: id})
+
+    if(findClient){
+        throw new AppError("Client not found" , 404)
+    }
+
+    userRepository.delete(id)
+
+    return {}
+}
