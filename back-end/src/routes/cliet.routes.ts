4b0c3411735a635203b811c4b227f696fc542b07
wesCarv <wcarvalho516@gmail.com>
2023-03-24 12:49:26 -0300

    feat: rotas de client concluidas

--- back-end/src/routes/cliet.routes.ts
+++ back-end/src/routes/cliet.routes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import { createClientController } from "../controllers/clients/createClientController";
+import { deleteClientController } from "../controllers/clients/deleteClientController";
+import { listClientController } from "../controllers/clients/listClientController";
+import { updateClientController } from "../controllers/clients/updateClientController";
 
 export const clientRoutes = Router()
 
 clientRoutes.post("" , createClientController)
+clientRoutes.get("" , listClientController)
+clientRoutes.patch("" , updateClientController)
+clientRoutes.delete("" , deleteClientController)
