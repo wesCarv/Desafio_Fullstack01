26853f82a8d9a0be176c2ef0b64718e339140363
wesCarv <wcarvalho516@gmail.com>
2023-03-29 13:02:28 -0300

    feat: criação da rota criar contato

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import { clientRoutes } from "./routes/cliet.routes"
+import { contactRoutes } from "./routes/contacts.routes"
 
 const app = express()
 app.use("/clients" , clientRoutes)
 app.use("/login" , clientRoutes)
+app.use("/contacts", contactRoutes)
 
 export default app
